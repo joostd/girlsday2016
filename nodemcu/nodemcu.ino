854a2356975a57cd3ce492b3cf498e6d59381660
Joost van Dijk <vandijk.joost@gmail.com>
2016-04-14 09:14:19 +0200

    add sample scripts

---
+++ nodemcu/nodemcu.ino
@@ -0,0 +1,35 @@
+#define BLYNK_PRINT Serial
+#include <SPI.h>
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+char auth[] = "yourtokenhere";
+
+WidgetLED led1(V1);
+
+const int buttonPin = D2;
+int triggerDetected=0;
+bool ledOn = true;
+
+static void pushedButtonEvent() {
+  triggerDetected=1;
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  Blynk.begin(auth, "yourssid", "yourpassword");
+  pinMode(buttonPin, INPUT);
+  attachInterrupt(buttonPin,pushedButtonEvent,FALLING);
+}
+
+void loop()
+{
+  Blynk.run();
+  if (triggerDetected) {
+    ledOn = !ledOn;
+    triggerDetected=0;
+    if( ledOn ) led1.on(); else led1.off();
+  }
+}
+
